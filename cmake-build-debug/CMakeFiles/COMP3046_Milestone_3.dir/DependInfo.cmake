7896e44e5ce736ec335fbc22db65cbce62406dae
FENGWENHAO <evanfungv@gmail.com>
2019-11-14 20:49:28 +0800

    cmake version downgrade to 3.14.5

--- cmake-build-debug/CMakeFiles/COMP3046_Milestone_3.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/COMP3046_Milestone_3.dir/DependInfo.cmake
@@ -4,9 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "C:/Working03/CLionProjects/comp3046_milestone3/main.cpp" "C:/Working03/CLionProjects/comp3046_milestone3/cmake-build-debug/CMakeFiles/COMP3046_Milestone_3.dir/main.cpp.obj"
+  "/Users/manhofung/CLionProjects/COMP3046-Milestone_3/main.cpp" "/Users/manhofung/CLionProjects/COMP3046-Milestone_3/cmake-build-debug/CMakeFiles/COMP3046_Milestone_3.dir/main.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "GNU")
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
--- cmake-build-debug/CMakeFiles/COMP3046_Milestone_3.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/COMP3046_Milestone_3.dir/cmake_clean.cmake
@@ -1,9 +1,7 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/COMP3046_Milestone_3.dir/main.cpp.obj"
-  "COMP3046_Milestone_3.exe"
-  "COMP3046_Milestone_3.exe.manifest"
+  "CMakeFiles/COMP3046_Milestone_3.dir/main.cpp.o"
   "COMP3046_Milestone_3.pdb"
-  "libCOMP3046_Milestone_3.dll.a"
+  "COMP3046_Milestone_3"
 )
 
 # Per-language clean rules from dependency scanning.
